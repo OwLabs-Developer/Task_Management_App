ab2d91c67a70c3b28baa6d33b47b4e2cdc62917a
chaad98 <faritzchad@yahoo.com>
2023-08-14 05:08:22 +0800

    First push and setup models database with its attributes

---
+++ db/migrate/20230813203922_create_tasks.rb
@@ -0,0 +1,14 @@
+class CreateTasks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text :description
+      t.date :due_date
+      t.integer :priority
+      t.boolean :status
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
